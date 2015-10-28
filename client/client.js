22e2011e5f7e619cc3875e10d749775bd2876745
Matthew Epler <matthewepler@gmail.com>
2015-10-28 16:31:10 -0400

    added exec and omxplayer command

--- client/client.js
+++ client/client.js
@@ -1,4 +1,5 @@
 var socket = require('socket.io-client')('http://shamebox.herokuapp.com');
+var exec = require('child_process').exec;
 
 socket.on('connect', function() {
 	console.log('yay, connected!');
@@ -8,4 +9,12 @@ socket.on('connect', function() {
 socket.on('shame', function(data) {
 	console.log('data rcvd: ');
 	console.log(data);
+	exec("omxplayer audio.mp3", function (error, stdout, stderr) {
+		console.log(stdout.toString('utf8');
+		console.log(stderr.toString('utf8');
+		if (error) {
+			console.log("ERROR: ");
+			console.log(error);
+		}
+	});
 });
