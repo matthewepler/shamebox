b923c6c310befaa4f74381d3b7239ad8725ec809
Matthew Epler <matthewepler@gmail.com>
2015-10-28 22:59:49 +0000

    added led and exit handler

--- client/client.js
+++ client/client.js
@@ -1,15 +1,22 @@
 var socket = require('socket.io-client')('http://shamebox.herokuapp.com');
 var exec = require('child_process').exec;
+var GPIO = require('onoff').Gpio,
+	led  = new GPIO(17, 'out');
 
 socket.on('connect', function() {
 	console.log('yay, connected!');
 	socket.send('hello');
+	led.writeSync(1);
+});
+
+socket.on('disconnect', function() {
+	led.writeSync(0);
 });
 
 socket.on('shame', function(data) {
 	console.log('data rcvd: ');
 	console.log(data);
-	exec("omxplayer audio.mp3", function (error, stdout, stderr) {
+	exec("omxplayer /home/pi/shamebox/client/womp.mp3", function (error, stdout, stderr) {
 		console.log(stdout.toString('utf8'));
 		console.log(stderr.toString('utf8'));
 		if (error) {
@@ -18,3 +25,17 @@ socket.on('shame', function(data) {
 		}
 	});
 });
+
+process.stdin.resume();
+
+function exitHandler(options, err) {
+	led.writeSync(0);
+	if (options.cleanup) console.log('clean');
+	if (err) console.log(err.stack);
+	if (options.exit) process.exit();
+}
+
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
