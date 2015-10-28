00cdc3f8891cf5a4fab9896b0d908bd4711f4cec
Matthew Epler <matthewepler@gmail.com>
2015-10-28 14:01:38 +0000

    server functionality working

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/shame', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+	var data = {
+		'text' : 'emitting shame'
+	};
+	io.emit('shame', data);
+});
+
 http.listen(3000, function() {
 	console.log('listening on *:3000');
 });
+
+io.on('connection', function(socket) {
+	console.log('user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+});
+
+
