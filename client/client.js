dadc3a40e6028e476d44f1b72f3395515e89096c
Matthew Epler <matthewepler@gmail.com>
2015-10-28 21:34:29 +0000

    audio working with increased gpu_mem in /boot/config.txt

--- client/client.js
+++ client/client.js
@@ -10,8 +10,8 @@ socket.on('shame', function(data) {
 	console.log('data rcvd: ');
 	console.log(data);
 	exec("omxplayer audio.mp3", function (error, stdout, stderr) {
-		console.log(stdout.toString('utf8');
-		console.log(stderr.toString('utf8');
+		console.log(stdout.toString('utf8'));
+		console.log(stderr.toString('utf8'));
 		if (error) {
 			console.log("ERROR: ");
 			console.log(error);
